c702710395d422d1b117074adc8cddc6f46af633
Geewon Lee <gwlee1991@gmail.com>
2017-12-24 20:55:04 -0800

    change to use hashrouter instead of browserrouter

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 
 const Root = () => (
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Root;
