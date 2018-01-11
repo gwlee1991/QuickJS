554b836714afb8aff0259a019e61aa19cf32620a
Geewon Lee <gwlee1991@gmail.com>
2018-01-10 21:30:38 -0800

    remove comments from users_controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,9 +2,6 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     
-    # if @user.email == ""
-    #   @user.errors.add :base, 'please enter email or log in with facebook'
-    #   render json: @user.errors.full_messages, status: 422
     if @user.save
       session[:session_token] = @user.session_token
       render "api/users/show.json.jbuilder"
