899725f5631035f13ec45f0c31f1d297392a95bc
Geewon Lee <gwlee1991@gmail.com>
2018-01-13 22:29:22 -0800

    add some more content to chap0 curriculum

--- curriculum/quickJS.js
+++ curriculum/quickJS.js
@@ -8,14 +8,15 @@ const curriculum = [
         subtitle: "3 ways to write code in Javscript",
         headers: [
           "1. The most simple way to run Javascript is to use an online compiler called repl.it.",
-          "2. The second way is to download node which is basically a framework for Javascript outside of the browser",
-          "3. The last way is to create Javascript files and then running the files. For the purpose of this course, we will run the files through node. Javascript files have an extension of .js (ex. document.js)"
+          "2. The second way is to download node which is basically a wrapper for Javascript outside of the browser",
+          "3. The last way is to create Javascript files and then running the files. For the purpose of this course, we will run the files through node. Javascript files have an extension of .js (ex. document.js)",
+          "4. You can download free text editors for writing Javascript in file with a .js extension.(think of it as ms word for programming languages)."
         ],
         contents: [
           [
-            "Navigate to https://repl.it/",
-            "On the part where it says `choose your language`, pick Javascript",
-            "start coding in Javascript"
+            "-Navigate to https://repl.it/",
+            "-On the part where it says `choose your language`, pick Javascript",
+            "-Start coding in Javascript"
           ],
           [
             "Navigate to https://nodejs.org/en/",
@@ -26,7 +27,11 @@ const curriculum = [
           ],
           [
             "create file with js extension",
-            "save and run by typing node filename.js"
+            "save and run by typing 'node filename.js'"
+          ],
+          [
+            "VS code(Visual Studio Code) => 'https://code.visualstudio.com/'",
+            "Atom => 'https://atom.io/"
           ]
         ]
       }
