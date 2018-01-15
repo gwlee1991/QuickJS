ef8483cccd05743b22f46974a48d057fd5d74ffb
Howard Feger <hfeger4@gmail.com>
2018-01-14 20:09:02 -0800

    finish chapter 3 part 1

---
+++ curriculum/chapter3.js
@@ -0,0 +1,76 @@
+const curriculum3 = [
+  {
+    title: "Chapter3",
+    topics: [
+      {
+        title: "JavaScript Arithmetic Operators",
+        videoID: "",
+        subtitle: "How to use Arithmetic Operators in JS",
+        headers: [
+          "1. List of JavaScript Arithmetic Operators and examples",
+          "2. The second way is to download node which is basically a wrapper for Javascript outside of the browser",
+          "3. The last way is to create Javascript files and then running the files. For the purpose of this course, we will run the files through node. Javascript files have an extension of .js (ex. document.js)",
+          "4. You can download free text editors for writing Javascript in file with a .js extension.(think of it as ms word for programming languages)."
+        ],
+        contents: [
+          [
+            "Here is a list of list of common operators that you would normally see on a calculator, ",
+            "as well as examples on how to use them in JavaScript.",
+            "",
+            "Operator : Description",
+            "+        : Addition",
+            "-        : Subtraction",
+            "*        : Multiplication",
+            "/        : Division",
+            "%        : Modulo",
+            "",
+            "Examples: ",
+            "Addition:",
+            "let x = 5 + 5",
+            "x is 10",
+            "JavaScript also gives the ability to increment a number by 1 using the '++' operator.",
+            "Increment:",
+            "var x = 4",
+            "x++",
+            "x is 5",
+            "Subtraction:",
+            "let x = 5 - 2",
+            "x is 3",
+            "As with addition, JavaScript also gives us the ability to decrement a number by 1 using the '--' operator.",
+            "Decrement:",
+            "let x = 4",
+            "x--;",
+            "x is 3",
+            "Multiplication:",
+            "let x = 10 * 10",
+            "x is 100",
+            "Division:",
+            "let x = 100 / 10",
+            "x is 10",
+            "Modulo gives us the remainder from division.",
+            "Modulo:",
+            "let x = 21 % 5",
+            "x is 1"
+          ],
+          [
+            "Navigate to https://nodejs.org/en/",
+            "Download nodejs. Use the recommended version",
+            "Once downloaded and installed, open terminal. (command + space) and then search for terminal",
+            "~$node",
+            "start coding"
+          ],
+          [
+            "create file with js extension",
+            "save and run by typing 'node filename.js'"
+          ],
+          [
+            "VS code(Visual Studio Code) => 'https://code.visualstudio.com/'",
+            "Atom => 'https://atom.io/"
+          ]
+        ]
+      }
+    ]
+  }
+];
+
+export default curriculum3;
