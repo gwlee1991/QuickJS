f0901627a06a62f14eb7e3a2011952f61161a080
Geewon Lee <gwlee1991@gmail.com>
2018-01-31 23:26:47 -0800

    add new chapter to curriculum

---
+++ curriculum/chapter3.js
@@ -0,0 +1,39 @@
+export const curriculum = {
+  title: "Chapter3",
+  subtitle: "Expressions, Statements, and Functions",
+  topics: [
+    {
+      title: "Expressions",
+      videoId: "",
+      subtitle: "",
+      headers: [
+        ""
+      ],
+      contents: [
+        [""]
+      ]
+    },
+    {
+      title: "Statements",
+      videoId: "",
+      subtitle: "",
+      headers: [
+        ""
+      ],
+      contents: [
+        [""]
+      ]
+    },
+    {
+      title: "Functions",
+      videoId: "",
+      subtitle: "",
+      headers: [
+        ""
+      ],
+      contents: [
+        [""]
+      ]
+    }
+  ]
+};
