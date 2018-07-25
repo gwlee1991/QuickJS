7d2f0b5cccb87f67cbb4c64c7e46e83d4d79f8ae
Geewon Lee <gwlee1991@gmail.com>
2018-07-25 00:15:33 -0700

    change format for chapter 2 curriculum

--- curriculum/quickJS.js
+++ curriculum/quickJS.js
@@ -3,7 +3,7 @@ import * as Chap1 from './chapter1';
 import * as Chap2 from './chapter2';
 
 const curriculum = [
-  Chap0.curriculum, Chap1.curriculum
+  Chap0.curriculum, Chap1.curriculum, Chap2.curriculum
 ];
 
 export default curriculum;
